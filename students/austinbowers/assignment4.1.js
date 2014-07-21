8ecc6725914f2084ca4c66b670985beadca9ff2b
Austin Bowers <bowers.austin@gmail.com>
2014-07-21 16:55:55 -0700

    Linted code added semi colons where needed and added strict logical operators

--- students/austinbowers/assignment4.1.js
+++ students/austinbowers/assignment4.1.js
@@ -5,22 +5,22 @@ function stringDelimiter (stringToSplit, separator) {
   var b = separator;
   var word = [];
   var wordConstructor = [];
-  var complete = []
+  var complete = [];
   
   for (i = 0; i < a.length; i++) {
     if (a.charAt(i) != separator) {
       word.push(a.charAt(i));
-    } else if (a.charAt(i) == separator) {
-      wordConstructor = word.join("")
-      complete.push(wordConstructor)
+    } else if (a.charAt(i) === separator) {
+      wordConstructor = word.join("");
+      complete.push(wordConstructor);
       while (word.length > 0) {
         word.pop();
       }
     }
   }
    complete = complete.filter(Boolean);
-   console.log(complete)
-   return complete
+   console.log(complete);
+   return complete;
 }
 
 run = stringDelimiter(sampleInput, "$");
